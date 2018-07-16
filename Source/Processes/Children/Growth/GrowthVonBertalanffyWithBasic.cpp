ecdf8beaae3099407f7b211e35a3fdea5e369af1
Craig44 <craig.marsh@bigbluedata.co.nz>
2018-07-16 14:08:27 +1200

    CHG: Growth Von Bert -> GrowthVonBertWith Mean, this is a fundamental change that current means a 'Growth' class is responsible for updating length and weight of agents, users can write any combination they wish in the future

---
+++ Source/Processes/Children/Growth/GrowthVonBertalanffyWithBasic.cpp
@@ -0,0 +1,132 @@
+/**
+ * @file GrowthVonBertalanffyWithBasic.cpp
+ * @author C.Marsh
+ * @github https://github.com/Craig44
+ * @date 13/07/2018
+ * @section LICENSE
+ *
+ *
+ * @section DESCRIPTION
+ *
+ */
+
+// headers
+#include "GrowthVonBertalanffyWithBasic.h"
+
+#include "Layers/Manager.h"
+#include "Utilities/RandomNumberGenerator.h"
+// namespaces
+namespace niwa {
+namespace processes {
+
+/**
+ * constructor
+ */
+GrowthVonBertalanffyWithBasic::GrowthVonBertalanffyWithBasic(Model* model) : Growth(model) {
+  parameters_.Bind<string>(PARAM_LINF_LAYER_LABEL, &l_inf_layer_label_, "Label for the numeric layer that describes mean L_inf through space", "", "");
+  parameters_.Bind<string>(PARAM_K_LAYER_LABEL, &k_layer_label_, "Label for the numeric layer that describes mean k through space", "", "");
+  parameters_.Bind<string>(PARAM_A_LAYER_LABEL, &a_layer_label_, "Label for the numeric layer that describes mean a in the weight calcualtion through space", "", "");
+  parameters_.Bind<string>(PARAM_B_LAYER_LABEL, &b_layer_label_, "Label for the numeric layer that describes mean b in the weight calcualtion through space", "", "");
+  parameters_.Bind<float>(PARAM_LINF, &l_inf_, "Label for the numeric layer that describes mean L_inf through space", "", 0);
+  parameters_.Bind<float>(PARAM_K, &k_, "Label for the numeric layer that describes mean k through space", "", 0);
+  parameters_.Bind<float>(PARAM_A, &a_, "Label for the numeric layer that describes mean a in the weight calcualtion through space", "", 0);
+  parameters_.Bind<float>(PARAM_B, &b_, "Label for the numeric layer that describes mean b in the weight calcualtion through space", "", 0);
+
+}
+
+// check users have defined spatial or non_spatial
+void GrowthVonBertalanffyWithBasic::DoValidate() {
+  if (parameters_.Get(PARAM_LINF_LAYER_LABEL)->has_been_defined() & parameters_.Get(PARAM_LINF)->has_been_defined())
+    LOG_ERROR_P(PARAM_LINF) << "You have specified both a value and a layer label for the 'L_inf' parameter, you must pick one or the other";
+  if (parameters_.Get(PARAM_K_LAYER_LABEL)->has_been_defined() & parameters_.Get(PARAM_K)->has_been_defined())
+    LOG_ERROR_P(PARAM_K) << "You have specified both a value and a layer label for the 'k' parameter, you must pick one or the other";
+  if (parameters_.Get(PARAM_A_LAYER_LABEL)->has_been_defined() & parameters_.Get(PARAM_A)->has_been_defined())
+    LOG_ERROR_P(PARAM_A) << "You have specified both a value and a layer label for the 'a' parameter, you must pick one or the other";
+  if (parameters_.Get(PARAM_B_LAYER_LABEL)->has_been_defined() & parameters_.Get(PARAM_B)->has_been_defined())
+    LOG_ERROR_P(PARAM_B) << "You have specified both a value and a layer label for the 'b' parameter, you must pick one or the other";
+
+  if (!parameters_.Get(PARAM_LINF_LAYER_LABEL)->has_been_defined() & !parameters_.Get(PARAM_LINF)->has_been_defined())
+    LOG_ERROR_P(PARAM_LABEL) << "You have not specified a value or a layer label for the 'L_inf' parameter, you must pick one of these options";
+  if (!parameters_.Get(PARAM_K_LAYER_LABEL)->has_been_defined() & !parameters_.Get(PARAM_K)->has_been_defined())
+    LOG_ERROR_P(PARAM_LABEL) << "You have not specified a value or a layer label for the 'k' parameter, you must pick one of these options";
+  if (!parameters_.Get(PARAM_A_LAYER_LABEL)->has_been_defined() & !parameters_.Get(PARAM_A)->has_been_defined())
+    LOG_ERROR_P(PARAM_LABEL) << "You have not specified a value or a layer label for the 'a' parameter, you must pick one of these options";
+  if (!parameters_.Get(PARAM_B_LAYER_LABEL)->has_been_defined() & !parameters_.Get(PARAM_B)->has_been_defined())
+    LOG_ERROR_P(PARAM_LABEL) << "You have not specified a value or a layer label for the 'b' parameter, you must pick one of these options";
+
+}
+
+
+// Build relationships between classes
+void GrowthVonBertalanffyWithBasic::DoBuild() {
+	// Get the layers if they have been defined
+  if (l_inf_layer_label_ != "") {
+    L_inf_layer_ = model_->managers().layer()->GetNumericLayer(l_inf_layer_label_);
+    if (!L_inf_layer_) {
+      LOG_ERROR_P(PARAM_LINF_LAYER_LABEL) << "could not find the layer '" << l_inf_layer_label_ << "', please make sure it exists and that it is type 'numeric'";
+    }
+  }
+  if (k_layer_label_ != "") {
+    k_layer_ = model_->managers().layer()->GetNumericLayer(k_layer_label_);
+    if (!k_layer_) {
+      LOG_ERROR_P(PARAM_K_LAYER_LABEL) << "could not find the layer '" << k_layer_label_ << "', please make sure it exists and that it is type 'numeric'";
+    }
+  }
+  if (a_layer_label_ != "") {
+    a_layer_ = model_->managers().layer()->GetNumericLayer(a_layer_label_);
+    if (!a_layer_) {
+      LOG_ERROR_P(PARAM_A_LAYER_LABEL) << "could not find the layer '" << a_layer_label_ << "', please make sure it exists and that it is type 'numeric'";
+    }
+  }
+  if (b_layer_label_ != "") {
+    b_layer_ = model_->managers().layer()->GetNumericLayer(b_layer_label_);
+    if (!b_layer_) {
+      LOG_ERROR_P(PARAM_B_LAYER_LABEL) << "could not find the layer '" << b_layer_label_ << "', please make sure it exists and that it is type 'numeric'";
+    }
+  }
+
+	// Check that the layers are all positive
+
+}
+
+
+/*
+ * This method is called at when ever an agent is created/seeded or moves. Agents will get a new/updated growth parameters
+ * based on the spatial cells of the process. This is called in initialisation/Recruitment and movement processes if needed.
+*/
+void  GrowthVonBertalanffyWithBasic::draw_growth_param(unsigned row, unsigned col, unsigned number_of_draws, vector<vector<float>>& vec) {
+  utilities::RandomNumberGenerator& rng = utilities::RandomNumberGenerator::Instance();
+  float mean_linf, mean_k, a, b;
+  if (L_inf_layer_)
+    mean_linf = L_inf_layer_->get_value(row, col);
+  else
+    mean_linf = l_inf_;
+
+  if (k_layer_)
+	  mean_k = k_layer_->get_value(row, col);
+  else
+    mean_k = k_;
+
+  if (a_layer_)
+    a = a_layer_->get_value(row, col);
+  else
+    a = a_;
+
+  if (b_layer_)
+    b = b_layer_->get_value(row, col);
+  else
+    b = b_;
+
+  vec.clear();
+	vec.resize(number_of_draws);
+
+	for (unsigned i = 0; i < number_of_draws; ++i) {
+	  vec[i].push_back(rng.lognormal(mean_linf, cv_));
+	  vec[i].push_back(rng.lognormal(mean_k, cv_));
+	  vec[i].push_back(a);
+	  vec[i].push_back(b);
+	}
+}
+
+} /* namespace processes */
+} /* namespace niwa */
---
+++ Source/Processes/Children/Growth/GrowthVonBertalanffyWithBasic.h
@@ -0,0 +1,60 @@
+/**
+ * @file GrowthVonBertalanffy.h
+ * @author C.Marsh
+ * @github https://github.com/Craig44
+ * @date 13/07/2018
+ * @section LICENSE
+ *
+ *
+ * @section DESCRIPTION
+ *
+ * This process is responsible for growing individuals length via the von-bert relationship and weight by the basic w = a * L^b method, so variaility is in the von-bert function not, a and b
+ */
+#ifndef SOURCE_PROCESSES_CHILDREN_GROWTH_VON_BERTALANFFY_WITH_BASIC_H_
+#define SOURCE_PROCESSES_CHILDREN_GROWTH_VON_BERTALANFFY_WITH_BASIC_H_
+
+// headers
+#include "Processes/Children/Growth/Growth.h"
+
+#include "Layers/Children/Numeric/Base/NumericLayer.h"
+// namespaces
+namespace niwa {
+namespace processes {
+
+class NumericLayer;
+/**
+ * Class definition
+ */
+class GrowthVonBertalanffyWithBasic : public Growth {
+public:
+  // methods
+  explicit GrowthVonBertalanffyWithBasic(Model* model);
+  virtual                     ~GrowthVonBertalanffyWithBasic() = default;
+  void                        DoValidate() override final;
+  void                        DoBuild() override final;
+  void                        DoReset() override final { };
+  void                        DoExecute() override final { };
+
+  void                        draw_growth_param(unsigned row, unsigned col, unsigned number_of_draws, vector<vector<float>>& vec) override final;
+
+protected:
+  std::string				          l_inf_layer_label_;
+  std::string				          k_layer_label_;
+  niwa::layers::NumericLayer* L_inf_layer_ = nullptr;
+  niwa::layers::NumericLayer* k_layer_ = nullptr;
+  std::string                 a_layer_label_;
+  std::string                 b_layer_label_;
+  niwa::layers::NumericLayer* a_layer_ = nullptr;
+  niwa::layers::NumericLayer* b_layer_ = nullptr;
+  float                       l_inf_;
+  float                       k_;
+  float                       a_;
+  float                       b_;
+
+
+};
+
+} /* namespace processes */
+} /* namespace niwa */
+
+#endif /* SOURCE_PROCESSES_CHILDREN_GROWTH_VON_BERTALANFFY_H_ */
