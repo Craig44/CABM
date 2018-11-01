335911a37c081768c0f674a3b484c2a725d600ca
Craig44 <craig.marsh@bigbluedata.co.nz>
2018-11-01 22:03:35 +1300

    ADD: a function for returning the index of ascending order of a vector used, in Mortality Effort Based.

--- Source/Utilities/Math.h
+++ Source/Utilities/Math.h
@@ -377,6 +377,21 @@ inline vector<double> elem_prod(vector<double>& x,vector<double>& y) {
     return result;
 }
 
+
+template <typename T>
+inline vector<size_t> sort_indexes(const vector<T> &v) {
+
+  // initialize original index locations
+  vector<size_t> idx(v.size());
+  iota(idx.begin(), idx.end(), 0);
+
+  // sort indexes based on comparing values in v
+  sort(idx.begin(), idx.end(),
+       [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
+
+  return idx;
+}
+
 } /* namespace math */
 } /* namespace utilities */
 } /* namespace niwa */
