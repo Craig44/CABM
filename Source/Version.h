159bff62dfce85ec79c84cc73c63999daa2d42b8
Craig44 <craig.marsh@bigbluedata.co.nz>
2018-11-03 09:51:02 +1300

    UPD: Documentation, User manual ADD: Estimation section about using minimsers ADD: ideas for new observation class

--- Source/Version.h
+++ Source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION aebc669955e1373a854889625dfdbc1920329c1d
-#define SOURCE_CONTROL_DATE "2018-11-01"
+#define SOURCE_CONTROL_REVISION 0d99703a37658808579a2c178fe2893c60d46bcd
+#define SOURCE_CONTROL_DATE "2018-11-02"
 #define SOURCE_CONTROL_YEAR "2018"
 #define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "09:04:42"
-#define SOURCE_CONTROL_VERSION "2018-11-01 09:04:42 UTC (rev. aebc669)"
+#define SOURCE_CONTROL_TIME "01:49:40"
+#define SOURCE_CONTROL_VERSION "2018-11-02 01:49:40 UTC (rev. 0d99703)"
 #endif
