aaf8e9fc5d463f75ce93b596a7004fe1143239c6
Craig44 <craig.marsh@bigbluedata.co.nz>
2019-02-25 16:08:02 +1300

    UPD: Version file

--- Source/Version.h
+++ Source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 7c0d8b94cbea3d32d1edc471ebdca53cede7deee
-#define SOURCE_CONTROL_DATE "2018-11-04"
-#define SOURCE_CONTROL_YEAR "2018"
-#define SOURCE_CONTROL_MONTH "November"
-#define SOURCE_CONTROL_TIME "09:52:43"
-#define SOURCE_CONTROL_VERSION "2018-11-04 09:52:43 UTC (rev. 7c0d8b9)"
+#define SOURCE_CONTROL_REVISION c69ca28987f0ad7d5146198f1da7a8213bcb029d
+#define SOURCE_CONTROL_DATE "2019-02-13"
+#define SOURCE_CONTROL_YEAR "2019"
+#define SOURCE_CONTROL_MONTH "February"
+#define SOURCE_CONTROL_TIME "01:42:13"
+#define SOURCE_CONTROL_VERSION "2019-02-13 01:42:13 UTC (rev. c69ca28)"
 #endif
