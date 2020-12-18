05c82165b596527c4ff5c5d9634ebbec596d62ac
Craig44 <Craig.Marsh@bigbluedata.co.nz>
2020-12-18 22:46:34 +1300

    ADD: MSE documentation section UPD: documentation

--- Source/Version.h
+++ Source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 6e15bb189d046c6da32f150f2d7bc0651769502a
-#define SOURCE_CONTROL_DATE "2020-12-01"
+#define SOURCE_CONTROL_REVISION bdeacb3b07affb8f0c728e7de1a6b147b4bb0148
+#define SOURCE_CONTROL_DATE "2020-12-14"
 #define SOURCE_CONTROL_YEAR "2020"
 #define SOURCE_CONTROL_MONTH "December"
-#define SOURCE_CONTROL_TIME "20:56:54"
-#define SOURCE_CONTROL_VERSION "2020-12-01 20:56:54 UTC (rev. 6e15bb1)"
+#define SOURCE_CONTROL_TIME "23:51:46"
+#define SOURCE_CONTROL_VERSION "2020-12-14 23:51:46 UTC (rev. bdeacb3)"
 #endif
