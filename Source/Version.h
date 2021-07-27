bb623879d0393f1d5dffa66fa613a14d343a64bd
Craig Marsh <marshc@localhost.localdomain>
2021-07-28 10:54:53 +1200

    ADD: python 39 move

--- Source/Version.h
+++ Source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 593e0ffc7f503774dca85f5ff37b9e26f68cfa3c
-#define SOURCE_CONTROL_DATE "2021-06-07"
+#define SOURCE_CONTROL_REVISION f092cfdcf2e53c865cc1ef819eea3e7fe3bd691e
+#define SOURCE_CONTROL_DATE "2021-07-06"
 #define SOURCE_CONTROL_YEAR "2021"
-#define SOURCE_CONTROL_MONTH "June"
-#define SOURCE_CONTROL_TIME "23:36:14"
-#define SOURCE_CONTROL_VERSION "2021-06-07 23:36:14 UTC (rev. 593e0ff)"
+#define SOURCE_CONTROL_MONTH "July"
+#define SOURCE_CONTROL_TIME "21:43:16"
+#define SOURCE_CONTROL_VERSION "2021-07-06 21:43:16 UTC (rev. f092cfd)"
 #endif
