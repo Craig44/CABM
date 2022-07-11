872fd026b2dab14f485b7c6254866d0e5c913f07
Craig Marsh <Craig.Marsh@niwa.co.nz>
2022-07-11 13:55:31 +1200

    UPD: manual

--- Source/Version.h
+++ Source/Version.h
@@ -1,10 +1,10 @@
 // This file is automatically built by the build system. Do not modify this file
 #ifndef VERSION_H_
 #define VERSION_H_
-#define SOURCE_CONTROL_REVISION 552351770a8e1286f907d21a703df8d88454ef81
+#define SOURCE_CONTROL_REVISION 9fe2ab305f0ea39105cdffff47e74056f2437664
 #define SOURCE_CONTROL_DATE "2022-07-10"
 #define SOURCE_CONTROL_YEAR "2022"
 #define SOURCE_CONTROL_MONTH "July"
-#define SOURCE_CONTROL_TIME "21:52:38"
-#define SOURCE_CONTROL_VERSION "2022-07-10 21:52:38 UTC (rev. 55235177)"
+#define SOURCE_CONTROL_TIME "23:56:20"
+#define SOURCE_CONTROL_VERSION "2022-07-10 23:56:20 UTC (rev. 9fe2ab30)"
 #endif
